73607c4a70bbb540e3b42e08d02eaea42f7e011a
Keeley Madigan <keeleysprouts@gmail.com>
2023-12-08 23:54:39 -0800

    array lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(){cats.push("Ralph")
+};
+function destructivelyPrependCat(){cats.unshift("Bob")
+};
+function destructivelyRemoveLastCat(){cats.pop(-0)
+};
+function destructivelyRemoveFirstCat(){cats.shift(0)
+};
+function appendCat() {
+    const sillycats = [...cats,"Broom"]
+    return sillycats
+};
+function prependCat() {
+    const dopeycats = ["Arnold",...cats]
+    return dopeycats
+};
+function removeLastCat(){
+    const dumbcats = cats.slice(0,-1)
+    return dumbcats
+};
+function removeFirstCat(){
+    const uglycats = cats.slice(-2)
+    return uglycats
+}
